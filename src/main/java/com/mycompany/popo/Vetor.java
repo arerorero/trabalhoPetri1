130d4d90606365e4196491f5a477baebd9663ada
Filippo <com.filippo@outlook.com>
2022-10-21 16:24:53 -0300

    quase perfeito

--- src/main/java/com/mycompany/popo/Vetor.java
+++ src/main/java/com/mycompany/popo/Vetor.java
@@ -20,9 +20,9 @@ public class Vetor {
     
     public void remove(int posicao) {
         this.vetor[posicao-1] = 0;
-        int casa = posicao-1;
-        for (int i = 0; i < this.tamanho-1; i++) {
-            if (this.vetor[casa+1] != 0 && this.vetor[casa] == 0){  //arrumar rebobinação da ordem de elementos
+        int casa = 0;
+        for (; casa < this.tamanho-1; casa++) {
+            if (this.vetor[casa+1] != 0 && this.vetor[casa] == 0){
                 this.vetor[casa] = this.vetor[casa+1];
                 this.vetor[casa+1] = 0;
             }   
--- src/main/java/com/mycompany/popo/popo.java
+++ src/main/java/com/mycompany/popo/popo.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.popo;
 
 import java.util.Scanner;
